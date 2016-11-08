25de99ea55d0d82570f445305bc8a4efe551f2d5
mimochat <mimoserver@gmail.com>
2016-11-08 18:26:49 +0900

    공백입력불가

--- web.js
+++ web.js
@@ -30,13 +30,13 @@ app.get( '/:code/:id/:name' , function(request, response) {
 	
 	console.log('express get room: '+room+' userId: ' + userId + ' name: '+ name );		
 	
-	fs.readFile( '/home/hosting_users/mimochat/apps/mimochat_mimochat/chat-ui.html', 'utf-8', function(error, data){ // cafe24
-	//fs.readFile( './chat-ui.html', 'utf-8', function(error, data){		
+	//fs.readFile( '/home/hosting_users/mimochat/apps/mimochat_mimochat/chat-ui.html', 'utf-8', function(error, data){ // cafe24
+	fs.readFile( './chat-ui.html', 'utf-8', function(error, data){		
 		response.writeHead( 200, { 'Content-Type' : 'text/html' } );
 		response.end( ejs.render(data, {
+			room: room,
 			id: userId,
-			name: name,
-			room: room
+			name: name			
 		}) );
 	}); 	
 	
@@ -58,7 +58,7 @@ io.sockets.on( 'connection', function(socket){
 		socket.join(data.room); 			//사용자가 입력한 방에 socket을 참여시킨다.
 		socket.room = data.room; 		//'room' 속성에 사용자가 입력한 방이름을 저장한다.
 		io.sockets.in( socket.room ).emit('message', {
-			id : 'MiMO',
+			name : 'MiMO',
 			message : data.name+'님이 ' + data.room+'방에 입장하셨습니다.',
 			date : prtDate
 		  });
@@ -68,7 +68,7 @@ io.sockets.on( 'connection', function(socket){
 	==============================*/
 	socket.on( 'message', function(data){
 		//'room' 속성값에 해당하는 방에 참여중인 Client에 메세지를 보낸다.
-		console.log( 'message> id : %s, msg : %s, date : %s', data.id, data.message, data.date );
+		console.log( 'message> id : %s, name : %s, msg : %s, date : %s', data.id, data.name, data.message, data.date );
 		io.sockets.in( socket.room ).emit('message', data); //public 통신 : io.sockets.emit(...);
 	});
 	
